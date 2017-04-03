c9e2bce41316068990d230227d332f8388d8f80c
Craig Austin <craig.austin@gmail.com>
2017-04-02 21:12:46 -0400

    Experimenting with routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Home = () => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+)
+
+const About = () => <div>About</div>
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.renderRoute()}
       </div>
     );
+  }
+
+  renderRoute() {
+    switch (window.location.pathname) {
+      case '/': return <Home />;
+      case '/about': return <About />;
+      default: throw new Error('bad route');
+    }
   }
 }
 
